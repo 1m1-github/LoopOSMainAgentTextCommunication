4589d73418a18bdb3d417a2b025429bc5c3084e6
1m1 <imi@1m1.io>
2026-04-24 09:24:40 -0600

    update

--- src/LoopOSMainAgentTextCommunication.jl
+++ src/LoopOSMainAgentTextCommunication.jl
@@ -6,16 +6,18 @@ import Base: take!, put!
 
 const AGENTGROUP = Dict{String,String}()
 
-const ROUTERSOCKET = Socket(ROUTER)
-const PUBSOCKET = Socket(PUB)
+const ROUTERSOCKET = Ref{Socket}()
+const PUBSOCKET = Ref{Socket}()
 
 function init(routerlocation, publocation)
-    @show ROUTERSOCKET, routerlocation 
-    bind(ROUTERSOCKET, routerlocation)
-    bind(PUBSOCKET, publocation)
+    ROUTERSOCKET[] = Socket(ROUTER)
+    PUBSOCKET[] = Socket(PUB)
+    @show ROUTERSOCKET[], routerlocation
+    bind(ROUTERSOCKET[], routerlocation)
+    bind(PUBSOCKET[], publocation)
     listen(RECEIVEMESSAGE)
     @whiletrue begin
-        frames = recv_multipart(ROUTERSOCKET)
+        frames = recv_multipart(ROUTERSOCKET[])
         to = String(frames[1])
         from = String(frames[2])
         haskey(AGENTGROUP, from) || continue
@@ -24,18 +26,18 @@ function init(routerlocation, publocation)
             put!(RECEIVEMESSAGE, message)
         elseif to == "group"
             group = AGENTGROUP[from]
-            send_multipart(PUBSOCKET, [group, from, message])
+            send_multipart(PUBSOCKET[], [group, from, message])
         elseif AGENTGROUP[from] == get(AGENTGROUP, to, "")
-            send_multipart(ROUTERSOCKET, [to, from, message])
+            send_multipart(ROUTERSOCKET[], [to, from, message])
         end
     end
 end
 
 send(socket, message, to) = send_multipart(socket, [to, Sys.username(), message])
 struct DirectMessage <: OutputPeripheral end
-put!(::DirectMessage, message::String, to::String) = send(ROUTERSOCKET, message, to)
+put!(::DirectMessage, message::String, to::String) = send(ROUTERSOCKET[], message, to)
 struct GroupMessage <: OutputPeripheral end
-put!(::GroupMessage, message::String, to::String="∀") = send(PUBSOCKET, message, to)
+put!(::GroupMessage, message::String, to::String="∀") = send(PUBSOCKET[], message, to)
 struct ReceiveMessage <: InputPeripheral
     channel::Channel{String}
 end
